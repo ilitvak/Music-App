51f455eb2232daf26f39f62a60fe052f8e991734
Irwin Litvak <irwin.litvak@gmail.com>
2020-07-27 13:23:58 -0400

    added icons for everything

--- js.js
+++ js.js
@@ -10,33 +10,41 @@ let currentMin = 0;
 let currentMax = 0;
 let currentTemp = 0;
 let currentHumidity = 0;
+let area = '';
+
 const minTempDiv = document.querySelector('.min p');
 const maxTempDiv = document.querySelector('.max p');
-const temperatureBox = document.querySelector('.temperature-box');
 const temperatureText = document.querySelector('.temperature p');
-let currentWeatherConditionText = document.querySelector('.temperature h2');;
-
-let count = 0;
-
-let newWord = '';
+const currentWeatherConditionText = document.querySelector('.temperature h2');
+const windContainer = document.querySelector('.icons-container .wind-text');
+const bottomIcons = document.querySelector('.bottom-icons');
+const sunriseText = document.querySelector('.sunrise-text');
+const sunsetText = document.querySelector('.sunset-text');
+const humidityText = document.querySelector('.humidity-text');
+const areaLookUp = document.querySelector('.area h2');
 
 btn.addEventListener('click', function(e){
 
     e.preventDefault();
-
-    console.log(e);
     userInputCity = document.querySelector('.zipCode').value;
 
     axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${userInputCity},${userInputState}&APPID=${apiKey}`)
         .then(function(response){
             console.log(response.data);
 
+            bottomIcons.classList.add('show');
+
             currentTemp = Math.floor((response.data.main.temp - 273.15) * 9/5 + 32);
-            currentHumidity = response.data.main.humidity;
             currentWeatherCondition = response.data.weather[0].description;
             currentMin = Math.floor((response.data.main.temp_min - 273.15) * 9/5 + 32);
             currentMax = Math.floor((response.data.main.temp_max - 273.15) * 9/5 + 32);
+
             wind = response.data.wind.speed;
+            currentHumidity = response.data.main.humidity;
+            sunrise = new Date(response.data.sys.sunrise).toLocaleTimeString("en-GB").slice(1,5);
+            sunset = new Date(response.data.sys.sunset).toLocaleTimeString("en-GB").slice(1,5);
+
+            area = response.data.name;
 
             if(currentTemp > 70){
                 document.querySelector('.sun').classList.add('show');
@@ -46,6 +54,11 @@ btn.addEventListener('click', function(e){
             currentWeatherConditionText.innerHTML = currentWeatherCondition;
             minTempDiv.innerHTML = ('Min Temp ' + currentMin + ' °');
             maxTempDiv.innerHTML = ('Max Temp ' + currentMax + ' °');
+            windContainer.innerHTML = wind + ' m/s';
+            sunriseText.innerHTML = sunrise + ' AM';
+            sunsetText.innerHTML = sunset + ' PM';
+            humidityText.innerHTML = currentHumidity + '%';
+            areaLookUp.innerHTML = area;
 
         })
         .catch(function(err){
